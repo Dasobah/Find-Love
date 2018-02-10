1f1e53ba4536f619c68c185dfde5f4e122226883
Doron Barasch <dasobah@gmail.com>
2018-02-10 11:45:50 -0800

    berry nice

---
+++ Find_Love/Assets/scripts/respawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour {
+
+    public GameObject player;
+    public Vector2 spawnLocation;
+
+	// Use this for initialization
+	void Start () {
+        spawnLocation = new Vector2(player.transform.position.x, player.transform.position.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.R))
+        {
+            player.transform.position = spawnLocation;
+            player.SetActive(true);
+
+        }
+	}
+}
