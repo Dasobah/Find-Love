57bbb20bd1c987c41285082075bdd170d44f99aa
hadesbane <ryandbarth@gmail.com>
2018-02-10 14:16:17 -0800

    branches merged

--- Find_Love/A Lover's Tale (Tentative)/Assets/scripts/movement.cs
+++ Find_Love/A Lover's Tale (Tentative)/Assets/scripts/movement.cs
@@ -2,41 +2,85 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movement : MonoBehaviour{
+public class movement : MonoBehaviour
+{
 
     public float speed;
     private Rigidbody2D rigid2D;
     public float jump;
-    private int jumpsLeft;
+    private int jumpsLeft = 1;
     public GameObject player;
 
-	// Use this for initialization
-	void Start () {
-        jumpsLeft = 1;
+    public float maxTime;
+    public float minTime;
+
+    // Use this for initialization
+    void Start()
+    {
         rigid2D = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float start = Time.time;
+
         rigid2D.velocity = new Vector2(0, rigid2D.velocity.y);
-        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             rigid2D.velocity = new Vector2(speed, rigid2D.velocity.y);
         }
-        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             rigid2D.velocity = new Vector2(-speed, rigid2D.velocity.y);
         }
-        if((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && jumpsLeft > 0)
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && jumpsLeft > 0)
         {
             Jump();
         }
-        
-	}
+        //stopping the jump
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.Space) || maxTime < Time.time - start)
+        {
+            if (rigid2D.velocity.y > 0F)
+            {
+                rigid2D.velocity = new Vector2(rigid2D.velocity.x, 0.1F);
+            }
+        }
+    }
 
     void Jump()
     {
         rigid2D.velocity = new Vector2(rigid2D.velocity.x, jump);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "groundTag" && jumpsLeft < 1)
+        {
+            jumpsLeft = 1;
+        }
+
+        if (collision.gameObject.tag == "deathTag")
+        {
+            player.SetActive(false);
+            GetComponent<respawn>().setDead();
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "groundTag" && jumpsLeft < 1)
+        {
+            jumpsLeft = 1;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "groundTag")
+        {
+            jumpsLeft = 0;
+        }
     }
 
     public int GetJumpsLeft()
--- Find_Love/A Lover's Tale (Tentative)/Assets/scripts/respawn.cs
+++ Find_Love/A Lover's Tale (Tentative)/Assets/scripts/respawn.cs
@@ -2,23 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class respawn : MonoBehaviour {
+public class respawn : MonoBehaviour
+{
 
+    public bool dead;
     public GameObject player;
     public Vector2 spawnLocation;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         spawnLocation = new Vector2(player.transform.position.x, player.transform.position.y);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyDown(KeyCode.R))
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
         {
             player.transform.position = spawnLocation;
             player.SetActive(true);
-
+            if (dead)
+            {
+                dead = false;
+            }
         }
-	}
+        if (dead && (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)))
+        {
+            player.transform.position = spawnLocation;
+            player.SetActive(true);
+            dead = false;
+        }
+    }
+
+    public void setDead()
+    {
+        dead = true;
+    }
 }
